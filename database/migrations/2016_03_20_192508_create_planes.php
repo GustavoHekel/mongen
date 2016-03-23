7e0282f3ff4075fbc648e75fde01ba324ea152da
Gustavo D. Hekel <gustavo.hekel@gmail.com>
2016-03-23 17:18:37 -0300

    database

---
+++ database/migrations/2016_03_20_192508_create_planes.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre' , 50)->unique();
+            $table->integer('visualizaciones');
+            $table->integer('descargas');
+            $table->integer('emails');
+            $table->integer('ediciones');
+            $table->integer('invitacion');
+            $table->float('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('planes');
+    }
+}
