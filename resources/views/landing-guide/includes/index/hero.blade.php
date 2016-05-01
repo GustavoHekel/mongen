e2247882d13a3dc68a76e702e2e51ce0ca001a46
Gustavo D. Hekel <gustavo.hekel@gmail.com>
2016-05-01 00:08:11 -0300

    cambios

--- resources/views/landing-guide/includes/index/hero.blade.php
+++ resources/views/landing-guide/includes/index/hero.blade.php
@@ -5,7 +5,10 @@
 			<div class="fh5co-intro-text">
 				<div class="fh5co-left-position">
 					<h2 class="animate-box">Create Awesome Things for Better Web</h2>
-					<p class="animate-box"><a href="https://vimeo.com/channels/staffpicks/93951774" class="btn btn-outline popup-vimeo btn-video"><i class="icon-play2"></i> Watch video</a> <a href="http://freehtml5.co" target="_blank" class="btn btn-primary">Visit FREEHTML5.co</a></p>
+					<p class="animate-box">
+						<!-- <a href="https://vimeo.com/channels/staffpicks/93951774" class="btn btn-outline popup-vimeo btn-video"><i class="icon-play2"></i> Watch video</a>  -->
+						<a href="http://freehtml5.co" target="_blank" class="btn btn-primary">Visit FREEHTML5.co</a>
+					</p>
 				</div>
 			</div>
 		</div>
